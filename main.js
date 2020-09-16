caa80f0ffe885dcae6d14dc8a395e9c6837b036f
Beardificent <cis.magito@gmail.com>
2020-09-16 14:35:15 +0200

    tested calling the find city as a function

--- main.js
+++ main.js
@@ -1,26 +1,35 @@
 
+const dayOne = document.querySelector('.dayOne');
+const dayTwo = document.querySelector('.dayTwo');
+const dayThree = document.querySelector('.dayThree');
+const dayFour = document.querySelector('.dayFour');
+const dayFive = document.querySelector('.dayFive');
 
 function getWeather () {
     console.log('button works');
-
-    let urlPartOne = 'https://api.openweathermap.org/data/2.5/forecast?q='
-    let city = document.getElementById('questionOne').value;
-    let urlParttwo = '&units=metric'
-    const apiKey = config.MY_KEY;
-
-
-
-    console.log(urlPartOne+ city + urlParttwo + '&APPID=' + apiKey);
-
-    /*fetch(url + city + '&APPID=' + apiKey)
-        .then((response) => response.json())
-        .then((data) => {
-    console.log(url + city + '&APPID=' + apiKey)
-
-     */
+    getLink ()
 
 
 }
+
+function getLink () {
+    let urlPartOne = 'https://api.openweathermap.org/data/2.5/forecast?q=';
+    let city = document.getElementById('questionOne').value;
+    let urlPartTwo = '&units=metric';
+
+    const apiKey = config.MY_KEY;
+    fetch(urlPartOne + city + urlPartTwo + '&APPID=' + apiKey)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(urlPartOne+ city + urlPartTwo + '&APPID=' + apiKey)
+            }
+        )
+
+}
+
+
+
+
 
 
 //EVENT LISTENERS
